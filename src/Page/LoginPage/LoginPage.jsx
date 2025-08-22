ca28b44f7f61f9e308f5d5f85ae5b83177b95730
Chewbi97 <jsmd97@hotmail.com>
2025-08-21 20:59:08 -0500

    trabajo de bootstrap para los estilos

--- src/Page/LoginPage/LoginPage.jsx
+++ src/Page/LoginPage/LoginPage.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import bootstrap from "bootstrap"
 //Estilos CSS//
 //import "./LoginPage.css"
 
@@ -6,18 +8,33 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div className="home-container">
-            <div className="login-box">
-                <h1>HOME</h1>
-                <Link to="/RegisterPage">
-                    <button className="login-btn">IR A REGISTRO</button>
-                </Link>
-                <Link to="/ForgetPage">
-                    <button className="login-btn">OLVIDE MI CONTRASEÑA</button>
-                </Link>
-                <Link to="/HooksGral">
-                    <button className="login-btn">HOOK GENERAL</button>
-                </Link>
+        <div className="home-container d-flex justify-content-center align-items-center vh-100">
+            <div className="login-box" style={{ width: "400px" }}>
+                <h2 className="text-center mb-4">INICIO DE SESIÓN</h2>
+                <form action="">
+                    <div className="mb-3">
+                        <label className="form-label d-flex-justify-content-center">Usuario</label>
+                        <input type="text" placeholder="Usuario o Correo Electrónico" className="user-input" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label d-flex-justify-content-center">Contraseña</label>
+                        <input type="password" placeholder="Contraseña" className="password-input" />
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div className="d-grid gap-3" style={{width:"250px"}}>
+                            <Link to="/RegisterPage">
+                                <button className="btn btn-primary width-100">IR A REGISTRO</button>
+                            </Link>
+                            <Link to="/ForgetPage">
+                                <button className="btn btn-primary width-100">OLVIDE MI CONTRASEÑA</button>
+                            </Link>
+                            <Link to="/HooksGral">
+                                <button className="btn btn-primary width-100">HOOK GENERAL</button>
+                            </Link>
+                        </div>
+                    </div>
+                </form>
+
             </div>
         </div>
     )
