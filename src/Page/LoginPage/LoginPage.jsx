d0380f07c31ca480684c1d0575e189a46a240470
Chewbi97 <jsmd97@hotmail.com>
2025-08-22 17:14:52 -0500

    Se termina de hacer la configuracion del login asi como su estetica con bootstrap y css, tambien se hace el formulario a falta de cuadrar cosas del style

--- src/Page/LoginPage/LoginPage.jsx
+++ src/Page/LoginPage/LoginPage.jsx
@@ -1,43 +1,69 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Swal from "sweetalert2";
-import bootstrap from "bootstrap"
-//Estilos CSS//
-//import "./LoginPage.css"
-
-
+import "./LoginPage.css"
 
 function Home() {
-    return (
-        <div className="home-container d-flex justify-content-center align-items-center vh-100">
-            <div className="login-box" style={{ width: "400px" }}>
-                <h2 className="text-center mb-4">INICIO DE SESIÓN</h2>
-                <form action="">
-                    <div className="mb-3">
-                        <label className="form-label d-flex-justify-content-center">Usuario</label>
-                        <input type="text" placeholder="Usuario o Correo Electrónico" className="user-input" />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label d-flex-justify-content-center">Contraseña</label>
-                        <input type="password" placeholder="Contraseña" className="password-input" />
-                    </div>
-                    <div className="d-flex justify-content-center align-items-center">
-                        <div className="d-grid gap-3" style={{width:"250px"}}>
-                            <Link to="/RegisterPage">
-                                <button className="btn btn-primary width-100">IR A REGISTRO</button>
-                            </Link>
-                            <Link to="/ForgetPage">
-                                <button className="btn btn-primary width-100">OLVIDE MI CONTRASEÑA</button>
-                            </Link>
-                            <Link to="/HooksGral">
-                                <button className="btn btn-primary width-100">HOOK GENERAL</button>
-                            </Link>
-                        </div>
-                    </div>
-                </form>
 
+    const [usuario, setUsuario] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const handleLogin = () => {
+
+        const usuarioCorrecto = "admin";
+        const passwordCorrecto = "1234";
+
+        if (usuario === usuarioCorrecto && password === passwordCorrecto) {
+            Swal.fire({
+                icon: "success", title: "Bienvenido", text: "Inicio de sesión exitoso", confirmButtonColor: "#856CF5"
+            });
+        } else {
+            Swal.fire({
+                icon: "error", title: "Error", text: "Usuario o contraseña incorrectos", confirmButtonColor: "#856CF5"
+            });
+        }
+    };
+
+    return (
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="card p-4 shadow" style={{ width: "350px" }}>
+                <h3 className="text-center mb-4">INICIO DE SESIÓN</h3>
+
+                <div className="mb-3">
+                    <label className="form-label fw-bold">Usuario</label>
+                    <input type="text" className="form-control" placeholder="Usuario o Correo Electrónico" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label fw-bold">Contraseña</label>
+                    <div className="input-group">
+                        <input
+                            type={showPassword ? "text" : "password"} className="form-control" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <button
+                            type="button" className="btn btn-outline-secondary" onClick={() => setShowPassword(!showPassword)}><i className={`bi ${showPassword ? "bi-eye-slash" : "bi-eye"}`}></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div className="d-grid gap-2">
+                    <button
+                        className="btn w-75 mx-auto text-white" style={{ backgroundColor: "#8F42E3", border: "2px solid #421e68", borderRadius: "8px", height: "30px", display: "flex", alignItems: "center", justifyContent: "center", padding: "0", marginBottom: "15px" }} onClick={handleLogin}>Iniciar Sesión</button>
+
+                    <Link to="/RegisterPage">
+                        <button className="btn custom-btn w-100 d-flex align-items-center justify-content-center">Registrarse</button>
+                    </Link>
+
+                    <Link to="/ForgetPage">
+                        <button className="btn custom-btn w-100 d-flex align-items-center justify-content-center">Olvidé Mi Contraseña</button>
+                    </Link>
+
+                    <Link to="/HooksGral">
+                        <button className="btn custom-btn w-100 d-flex align-items-center justify-content-center">Hook General</button>
+                    </Link>
+                </div>
             </div>
         </div>
-    )
-};
+    );
+}
 
 export default Home;
--- src/Page/RegisterPage/RegisterPage.jsx
+++ src/Page/RegisterPage/RegisterPage.jsx
@@ -1,14 +1,49 @@
 import { Link } from "react-router-dom";
 
 
-function REGISTER(){
-    return(
-        <div>
-            <h1>REGISTRO</h1>
-            <Link to ="/">
-                <button>VOLVER AL INICIO</button>
-            </Link>
-            
+function REGISTER() {
+    return (
+        <div className="container d-flex justify-content-center align-items-start" style={{ minHeight: "100vh", paddingTop: "20px", paddingBottom: "20px", overflowY: "auto" }}>
+            <div className="card p-4 shadow" style={{ width: "500px" }}>
+                <h2 className="text-center mb-4">REGISTRO PARA NUEVO USUARIO</h2>
+                <div className="mb-3">
+                    <label className="form-label fw-bold">Nombre</label>
+                    <input type="text" className="form-control mb-3" placeholder="Juan"></input>
+                    <label className="form-label fw-bold">Apellido</label>
+                    <input type="text" className="form-control mb-3" placeholder="Peréz"></input>
+                    <label className="form-label fw-bold">Pais</label>
+                    <input type="text" className="form-control mb-3" placeholder="Mexico"></input>
+                    <label className="form-label fw-bold">Documento</label>
+                    <div className="d-flex">
+                        <select className="form-select me-2" style={{ width: "100px" }}>
+                            <option>C.C</option>
+                            <option>T.I</option>
+                            <option>C.E</option>
+                        </select>
+                        <input type="text" className="form-control mb-3" placeholder="123456789"></input>
+                    </div>
+                    <label className="form-label fw-bold">Telefono</label>
+                    <input type="text" className="form-control mb-3" placeholder="355555555"></input>
+                    <label className="form-label fw-bold">Correo Electrónico</label>
+                    <input type="text" className="form-control mb-3" placeholder="juan@email.com"></input>
+                    <label className="form-label fw-bold">Contraseña</label>
+                    <input type="text" className="form-control mb-3" placeholder="8 Caracteres, al menos una mayuscula, un numero y un caracter especial" style={{ fontSize: "12px" }}></input>
+                    <label className="form-label fw-bold">Confirmar Contraseña</label>
+                    <input type="text" className="form-control mb-3" placeholder="8 Caracteres, al menos una mayuscula, un numero y un caracter especial"></input>
+
+                </div>
+                <div className="d-flex flex-column align-items-center mt-3">
+                    <Link to="/">
+                        <button className="btn btn-primary mb-2" style={{ width: "150px" }}>Registrar</button>
+                    </Link>
+                    <Link to="/">
+                        <button className="btn btn-light border" style={{ width: "150px" }}>Cancelar</button>
+                    </Link>
+                </div>
+            </div>
+
+
+
         </div>
     )
 };
