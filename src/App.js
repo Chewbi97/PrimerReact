3d889bb5f8fe946a8a9c7f21f38f92ff9158d99c
Chewbi97 <jsmd97@hotmail.com>
2025-08-25 17:02:11 -0500

    Finalización del login y sus extensiones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
+//Publicas//
 import LoginPage from "./Page/LoginPage/LoginPage"
 import RegisterPage from "./Page/RegisterPage/RegisterPage"
 import ForgetPage from "./Page/ForgetPage/ForgetPage"
+import ResetPasswordPage from "./Page/ResetPasswordPage/ResetPasswordPage"
 
 //Import para HOOKS
 import HooksGral from "./Playground/HooksGral"
 import HookUseState from "./Playground/HookUseState" 
-
+import UseEffectPlay from "./Playground/UseEffectPlay"
+import UseStatePlay from "./Playground/UseStatePlay"
+import UseRefPlay from "./Playground/UseRefPlay"
 
 function App() {
   
@@ -17,11 +21,16 @@ function App() {
         <Route path="/" element={<LoginPage/>} /> 
         <Route path="/ForgetPage" element={<ForgetPage/>} />
         <Route path="/RegisterPage" element={<RegisterPage/>} />
+        <Route path="/ResetPasswordPage" element={<ResetPasswordPage/>} />
 
         {/* Rutas para Hooks*/}
         <Route path="/HooksGral" element={<HooksGral/>} />
         <Route path="/usestate" element={<HookUseState/>} />
-        
+        <Route path="/usestate" element={<UseStatePlay />} />
+        <Route path="/useeffect" element={<UseEffectPlay />} />
+        <Route path="/useref" element={<UseRefPlay />} />
+
+       ¿
       
       </Routes>
       </BrowserRouter>
--- src/Page/RegisterPage/RegisterPage.jsx
+++ src/Page/RegisterPage/RegisterPage.jsx
@@ -1,21 +1,55 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import "./RegisterPage.css"
 
 function REGISTER() {
+
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [confirmError, setConfirmError] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const validatePassword = (value) => {
+        setPassword(value);
+
+        // Validador //
+        const hasUpperCase = /[A-Z]/.test(value);
+        const hasLowerCase = /[a-z]/.test(value);
+        const hasNumber = /[0-9]/.test(value);
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+        const hasMinLength = value.length >= 8;
+
+        if (!hasUpperCase || !hasLowerCase || !hasNumber || !hasSpecialChar || !hasMinLength) {
+            setError("Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.");
+        } else {
+            setError(""); // ✅ Cumple con todo
+        }
+    };
+    const validateConfirmPassword = (value) => {
+        setConfirmPassword(value);
+
+        if (value !== password) {
+            setConfirmError("Las contraseñas no coinciden.");
+        } else {
+            setConfirmError("");
+        }
+    };
     return (
-        <div className="container d-flex justify-content-center align-items-start" style={{ minHeight: "100vh", paddingTop: "20px", paddingBottom: "20px", overflowY: "auto" }}>
+        <div className="container d-flex justify-content-center align-items-start"
+            style={{ minHeight: "100vh", paddingTop: "20px", paddingBottom: "20px", overflowY: "auto" }}>
             <div className="card p-4 shadow" style={{ width: "500px" }}>
                 <h2 className="text-center mb-4">REGISTRO PARA NUEVO USUARIO</h2>
                 <div className="mb-3">
                     <label className="form-label fw-bold">Nombre</label>
-                    <input type="text" className="form-control mb-3" placeholder="Juan"></input>
+                    <input type="text" className="form-control mb-3" placeholder="Nombres de Usuario"></input>
                     <label className="form-label fw-bold">Apellido</label>
-                    <input type="text" className="form-control mb-3" placeholder="Peréz"></input>
+                    <input type="text" className="form-control mb-3" placeholder="Apellidos de Usuario"></input>
                     <label className="form-label fw-bold">Pais</label>
-                    <input type="text" className="form-control mb-3" placeholder="Mexico"></input>
+                    <input type="text" className="form-control mb-3" placeholder="Colombia"></input>
                     <label className="form-label fw-bold">Documento</label>
                     <div className="d-flex">
-                        <select className="form-select me-2" style={{ width: "100px" }}>
+                        <select className="form-select me-2" style={{ width: "75px", height: "35px" }}>
                             <option>C.C</option>
                             <option>T.I</option>
                             <option>C.E</option>
@@ -25,26 +59,63 @@ function REGISTER() {
                     <label className="form-label fw-bold">Telefono</label>
                     <input type="text" className="form-control mb-3" placeholder="355555555"></input>
                     <label className="form-label fw-bold">Correo Electrónico</label>
-                    <input type="text" className="form-control mb-3" placeholder="juan@email.com"></input>
-                    <label className="form-label fw-bold">Contraseña</label>
-                    <input type="text" className="form-control mb-3" placeholder="8 Caracteres, al menos una mayuscula, un numero y un caracter especial" style={{ fontSize: "12px" }}></input>
+                    <input type="text" className="form-control mb-3" placeholder="usuario@e-mail.com"></input>
+                    <div>
+                        <label className="form-label fw-bold">Contraseña</label>
+                        <div className="input-group mb-2">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                className="form-control"
+                                value={password}
+                                onChange={(e) => validatePassword(e.target.value)}
+                                placeholder="Escriba su Contraseña"
+                            />
+                            <button
+                                type="button"
+                                className="btn btn-outline-secondary"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                <i className={`bi ${showPassword ? "bi-eye-slash" : "bi-eye"}`}></i>
+                            </button>
+                        </div>
+                        <div style={{ minHeight: "20px" }}>
+                        {error && <small className="text-danger">{error}</small>}
+                        </div>
                     <label className="form-label fw-bold">Confirmar Contraseña</label>
-                    <input type="text" className="form-control mb-3" placeholder="8 Caracteres, al menos una mayuscula, un numero y un caracter especial"></input>
-
-                </div>
-                <div className="d-flex flex-column align-items-center mt-3">
-                    <Link to="/">
-                        <button className="btn btn-primary mb-2" style={{ width: "150px" }}>Registrar</button>
-                    </Link>
-                    <Link to="/">
-                        <button className="btn btn-light border" style={{ width: "150px" }}>Cancelar</button>
-                    </Link>
+                    <div className="input-group mb-2">
+                        <input
+                            type={showConfirmPassword ? "text" : "password"}
+                            className="form-control"
+                            value={confirmPassword}
+                            onChange={(e) => validateConfirmPassword(e.target.value)}
+                            placeholder="Confirme su Contraseña"
+                        />
+                        <button
+                            type="button"
+                            className="btn btn-outline-secondary"
+                            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                        >
+                            <i className={`bi ${showConfirmPassword ? "bi-eye-slash" : "bi-eye"}`}></i>
+                        </button>
+                    </div>
+                    <div style={{ minHeight: "20px" }}>
+                    {confirmError && <small className="text-danger">{confirmError}</small>}
+                    </div>
                 </div>
             </div>
-
-
-
+            <div className="d-flex flex-column align-items-center mt-3">
+                <Link to="/">
+                    <button className="btn custom-btn w-140 d-flex align-items-center justify-content-center" style={{ width: "150px" }}>Registrar</button>
+                </Link>
+                <Link to="/">
+                    <button className="custom-bttn w-140 d-flex align-items-center justify-content-center" style={{ width: "150px" }}>Cancelar</button>
+                </Link>
+            </div>
         </div>
+
+
+
+        </div >
     )
 };
 
